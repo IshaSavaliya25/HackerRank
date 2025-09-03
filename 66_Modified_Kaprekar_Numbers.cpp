b96e81c32a8de1b31a75279826895bd6e6c6c8d7
Isha Savaliya <155796476+IshaSavaliya25@users.noreply.github.com>
2025-09-03 18:41:02 +0530

    66_Modified_Kaprekar_Numbers.cpp

---
+++ 66_Modified_Kaprekar_Numbers.cpp
@@ -0,0 +1,167 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'kaprekarNumbers' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER p
+ *  2. INTEGER q
+ */
+
+void kaprekarNumbers(int p, int q) {
+    int found = 0;
+    
+    for (int i = p; i <= q; i++) {
+        long long sq = (long long)i * (long long)i;
+        
+        int d = 0, temp = i;
+        while (temp > 0) {
+            d++;
+            temp /= 10;
+        }
+        
+        long long power = 1;
+        for (int j = 0; j < d; j++) {
+            power *= 10;
+        }
+        
+        long long R = sq % power;
+        long long L = sq / power;
+        
+        if (L + R == i) {
+            printf("%d ", i);
+            found = 1;
+        }
+    }
+    
+    if (!found) {
+        printf("INVALID RANGE");
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+    int p = parse_int(ltrim(rtrim(readline())));
+
+    int q = parse_int(ltrim(rtrim(readline())));
+
+    kaprekarNumbers(p, q);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
